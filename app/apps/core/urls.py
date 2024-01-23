75f79e67a24c4d9b0e7fdd8916a394a8b6e25dc5
Piotr <piotr.zegarekk@gmail.com>
2024-01-23 18:16:26 +0100

    Added devices urls

--- app/apps/core/urls.py
+++ app/apps/core/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home-page"),
+    path("devices/add", views.DevicesAddView.as_view(), name="devices-add-page"),
+    path("devices/<str:dev_type>", views.DevicesView.as_view(), name="devices-page"),
 ]
--- app/apps/core/views.py
+++ app/apps/core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.shortcuts import render
 from django.views import View
 
@@ -9,3 +10,22 @@ class HomeView(LoginRequiredMixin, View):
 
     def post():
         pass
+
+
+class DevicesView(LoginRequiredMixin, View):
+    def get(self, request, dev_type):
+        if dev_type not in ["consume", "store", "produce"]:
+            raise Http404("Object does not exist")
+
+        return render(
+            request,
+            "core/devices.html",
+            {
+                "type": dev_type,
+            },
+        )
+
+
+class DevicesAddView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, "core/devices-add.html")
